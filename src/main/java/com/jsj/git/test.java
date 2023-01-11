16708c7713af0d3b036550bc68ac620efb76bbe0
ee”ccy <ccy@email.com>
2023-01-11 16:05:57 +0800

    first commit

---
+++ src/main/java/com/jsj/git/test.java
@@ -0,0 +1,13 @@
+package com.jsj.git;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/1/11 16:01
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
